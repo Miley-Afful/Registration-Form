e15bb25924538be2f336cf3c07d2b91957f2337e
Miley-Afful <mileyafhibarhafful@gmail.com>
2024-09-16 12:15:59 +0000

    creating registration form

--- scripts/reister.js
+++ scripts/reister.js
@@ -1,10 +1,10 @@
 // Select form by id
 const registerForm = document.getElementById ('refister-form');
 // Handle form submit event
-registerForm.onsubmit = funtion () {
-    // collect user name
-    // collect email
-    // collect password 
-    // save user information
-    // send confirmation email
+registerForm.onsubmit = funtion() {
+    //Collect user name
+    //Collect email
+    //Collect password 
+    //Save user information
+    //Send confirmation email
 }
